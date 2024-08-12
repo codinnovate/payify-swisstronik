c34cecacb1a63e612f856911cb86c93330547e9d
Itunu Adeyemi <adeyemis710@gmail.com>
2024-08-12 21:14:37 +0100

    main

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
+module.exports = {
+  solidity: "0.8.24",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
